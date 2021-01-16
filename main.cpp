25c561f81888f9cd5135347ff3cde90ecb5bd192
nishatmahi <77322410+nishatmahi@users.noreply.github.com>
2021-01-16 14:32:33 +0600

    programme

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t,i;
+    cin>>t;
+    while(t--){
+        int m,n;
+        cin>>n>>m;
+        if(n%m==0){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+
+    }
+    return 0;
+}
